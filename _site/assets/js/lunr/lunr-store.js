fdcc03213c87f7109fda035c999e1d808a07e867
Jérôme Morissard <morissardj@gmail.com>
2022-09-14 21:40:14 +0200

    Generate Production website

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,36 +2,36 @@ var store = [{
         "title": "App Technologies",
         "excerpt":"Since 10 years i have seen a lot of cross-plateform technologies. Every time, there is some reasons to select an alternative technology. Reduce the cost of the App Use the internal resources to develop the App Recruitment too complex For several years, we the maturity of some challengers, i was...","categories": ["AppStore","Cross Platform","Flutter","React Native"],
         "tags": ["Xamarin","React Native","Cordova","Web App","Flutter","Native"],
-        "url": "/appstore/cross%20platform/flutter/react%20native/app-technologies/",
+        "url": "/pages/leverdeterre/appscan/appstore/cross%20platform/flutter/react%20native/app-technologies/",
         "teaser": null
       },{
         "title": "AppScore - definition",
         "excerpt":"To be more objective to judge a App and be able to compare Apps, we have created an AppScore. We are not going to rate the features of the App but we wanted to be realist of the strenghts and the weaknesses of the App. AppScore computation can be devided...","categories": ["AppStore","AppScore"],
         "tags": [],
-        "url": "/appstore/appscore/appscore-definition/",
+        "url": "/pages/leverdeterre/appscan/appstore/appscore/appscore-definition/",
         "teaser": null
       },{
         "title": "AppScan - data collection",
         "excerpt":"I started to work on that project some years ago. I was working on an other project RocketSkill. I was trying to generate a global vision of the AppStore to give some advises to new iOS developers. What libraries are important to know? How to improve my learning curve on...","categories": ["AppStore","Product"],
         "tags": ["ipa","scan"],
-        "url": "/appstore/product/data-collection/",
+        "url": "/pages/leverdeterre/appscan/appstore/product/data-collection/",
         "teaser": null
       },{
         "title": "Apple Transport Security or not",
         "excerpt":"On iOS, the network exchanges is an important layer of the security. This layer is used by the App when performing network requests with : URLSession, URLConnection, WKWebView loading, Thirdparty librairies (Alamofire, AFNetworking) Using our current database we have discovered that 48% of the iOS app have remove or disable...","categories": ["AppStore","Security"],
         "tags": ["NSAllowsArbitraryLoads"],
-        "url": "/appstore/security/app-transport-security/",
+        "url": "/pages/leverdeterre/appscan/appstore/security/app-transport-security/",
         "teaser": null
       },{
         "title": "Crypto, Bitcoin, Wallet Apps - AppScan audits",
         "excerpt":"Apps relative to crypto are very recent. We wanted to study those Apps to make an smart article about technical stacks &amp; good practices. What are the good things? What are the bad things in those about? What are the important points if you want to start to build a...","categories": ["AppAudits"],
         "tags": [],
-        "url": "/appaudits/crypto-bitcoin-wallet-apps/",
+        "url": "/pages/leverdeterre/appscan/appaudits/crypto-bitcoin-wallet-apps/",
         "teaser": null
       },{
         "title": "Big data & Smart queries",
         "excerpt":"If we use all the metadata we can do smart queries. To help to take product decisions On top 100 of “Lifestyle” category of AppStore, How many Apps are optimized for iPad? On top 100 of “Lifestyle” category of AppStore, What is the minimal iOS version? On all top 10,...","categories": ["AppAudits","AppStore","AppScan","Metadata"],
         "tags": [],
-        "url": "/appaudits/appstore/appscan/metadata/smart-queries/",
+        "url": "/pages/leverdeterre/appscan/appaudits/appstore/appscan/metadata/smart-queries/",
         "teaser": null
       }]
